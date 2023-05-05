7ab78f8e85164f7ff19e9168dac2b41a6e0f97bd
nodarchik <chikadzee@gmail.com>
2023-05-05 13:53:31 +0400

    Reformated project

--- Frontend/src/app/app.module.ts
+++ Frontend/src/app/app.module.ts
@@ -1,13 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
-import { ReactiveFormsModule } from '@angular/forms'; // Add this import
+import { ReactiveFormsModule } from '@angular/forms';
+import { RouterModule, Routes } from '@angular/router'; // Add this import
 
 import { AppComponent } from './app.component';
 import { MainPageComponent } from './main-page/main-page.component';
 import { ProductAddComponent } from './product-add/product-add.component';
 import { ProductListComponent } from './product-list/product-list.component';
 import { PaginationComponent } from './pagination/pagination.component';
+
+const appRoutes: Routes = [
+  { path: '', component: MainPageComponent },
+  { path: 'add-product', component: ProductAddComponent },
+];
 
 @NgModule({
   declarations: [
@@ -20,7 +26,8 @@ import { PaginationComponent } from './pagination/pagination.component';
   imports: [
     BrowserModule,
     HttpClientModule,
-    ReactiveFormsModule, // Add this to the imports array
+    ReactiveFormsModule,
+    RouterModule.forRoot(appRoutes), // Add this to the imports array
   ],
   providers: [],
   bootstrap: [AppComponent],
